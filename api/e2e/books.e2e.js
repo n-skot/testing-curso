f50354ece77cde40e460773744803c52b43bd400
n-skot <ricardo.nskot@gmail.com>
2024-06-15 18:42:17 -0600

    .github, e2e, integrattion

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,38 +1,49 @@
 const request = require('supertest');
-
-const { bookManyGenerate } = require('../src/fakes/book.fake');
-
-const mockGetAll = jest.fn();
-
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => { },
-})));
-
+const { MongoClient } = require('mongodb');
+const { config } = require('../src/config/')
+const { booksE2e } = require('../src/fakes/book.fake')
 const createApp = require('../src/app');
+
+const DB_NAME = config.dbName;
+const MONGO_URL = config.dbUrl;
 
 describe('test for books', () => {
   let app;
   let server;
+  let database;
+  let client;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(5000);
-    jest.clearAllMocks();
-  });
-
-  describe('test for GET api/v1/books', () => {
-    test('test for books', async () => {
-      const fakeBooks = bookManyGenerate(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
-      const response = await request(app).get('/api/v1/books');
-      expect(response.status).toEqual(200);
-      console.log(response.body);
-      expect(response.body.length).toEqual(fakeBooks.length);
-    });
+    client = new MongoClient(
+      MONGO_URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
+    await client.close();
+  });
+
+  describe('test for GET api/v1/books', () => {
+    test('test for books', async () => {
+      //Arrange
+      const fakeBooks = booksE2e(6);
+      const seed = await database.collection('books').insertMany(fakeBooks);
+      //Act
+      const response = await request(app).get('/api/v1/books');
+      //Assert
+      expect(response.status).toEqual(200);
+      console.log(seed);
+      console.log(response.body);
+      expect(response.body.length).toEqual(seed.insertedCount);
+    });
   });
 });
--- api/src/fakes/book.fake.js
+++ api/src/fakes/book.fake.js
@@ -20,4 +20,20 @@ const bookManyGenerate = (size) => {
   return booksArray;
 };
 
-module.exports = { bookGenerate, bookManyGenerate };
+const booksE2e = (size) => {
+  const filter = size ?? 5;
+  const booksArray = [];
+  for (let books = 1; books <= filter; books += 1) {
+    const booksFakes = {
+      numSerie: faker.string.uuid(),
+      name: faker.commerce.product(),
+      year: faker.date.past(),
+      price: faker.commerce.price(),
+      author: faker.person.firstName()
+    };
+    booksArray.push(booksFakes);
+  }
+  return booksArray;
+}
+
+module.exports = { bookGenerate, bookManyGenerate, booksE2e };
